266b094278d418cdc03b51734bc455e67a15e2f1
Casper Hedegaard <averagejoedk@gmail.com>
2017-11-24 10:24:57 +0100

    Snyt Model

---
+++ models/Snyt.model.js
@@ -0,0 +1,15 @@
+/* jslint node: true */
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var SnytSchema = new Schema({
+    subject: String,
+    category: String,
+    text: String,
+    user: String,
+    created: {type: Date, default: Date.now}
+});
+
+module.exports = mongoose.model('Snyt', SnytSchema);
