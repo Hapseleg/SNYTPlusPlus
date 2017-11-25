8b8770d5db0368db4c0cfb02d74f1f760c1913c6
Casper Hedegaard <averagejoedk@gmail.com>
2017-11-25 13:08:45 +0100

    US1 test + funktionalitet i app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var express = require('express'),
     Snyt = require('./models/Snyt.model');
 
 var mongoUrl = 'mongodb://snytfix:snytfix@ds115166.mlab.com:15166/snytplusplus';
+mongoose.Promise = global.Promise;
 
 var app = express();
 
@@ -19,11 +20,11 @@ mongoClient.connect(mongoUrl, function (err, db) {
     }
     console.log("Connected successfully to server");
     app.Snyt = db.collection('Snyt');
-    db.ensureIndex('subject', 'category', 'text', 'user','created','edok', function (err) {
-        if (err) {
-            throw err
-        }
-    });
+    // db.ensureIndex('subject', 'category', 'text', 'user','created','edok', function (err) {
+    //     if (err) {
+    //         throw err
+    //     }
+    // });
 });
 
 mongoose.connect(mongoUrl, {
@@ -39,9 +40,32 @@ app.get('/',function (req,res) {
 
 });
 
+app.get('/search', function(req, res) {
+    Snyt.find({}).exec(function(err, doc) {
+        if(err) {
+            throw err;
+        } else {
+            res.json(doc);
+        }
+    });
+});
 
+app.get('/search/:text', function(req, res) {
+    var reg = new RegExp(".*" + req.params.text + ".*", "i");
+    Snyt.find({"$or":[{"subject": reg}, {"text": reg}]}).exec(function(err, doc) {
+        if(err) {
+            throw err;
+        } else {
+            res.json(doc);
+        }
+    });
+});
+
+app.post('/search', function(req, res) {
+
+});
 
 //Start it up!!! WOOP WOOP WOOP SNYT++ 4 lyfe
 app.listen(1337);
 
-module.exports = mongoClient;
+// module.exports = mongoClient;
