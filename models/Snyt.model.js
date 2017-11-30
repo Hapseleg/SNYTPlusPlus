d57ba8b1d46acfcba10cd8f8409e016c6a25d5ff
Rune <Znox@live.dk>
2017-11-30 08:13:22 +0100

    Revert "Revert "us10 mm""
    This reverts commit d4ca695c4c74686a4411549e456e231d3c4ecf9d.

--- models/Snyt.model.js
+++ models/Snyt.model.js
@@ -3,15 +3,32 @@
 
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var User = require('./User.model');
 
+// var s = function() {
+//     User.find().exec().then(function (user) {
+//         var a =[];
+//         for (var j in user) {
+//             console.log(j + '_:_ '+user[j]);
+//         }
+//         return user;
+//     });
+// };
+//
+// var ss = function () {
+//     var a = [];
+//     for(var i in s()){
+//         console.log(i)
+//     }
+// };
 var SnytSchema = new Schema({
     subject: String,
     category: String,
     text: String,
     user: String,
     created: {type: Date, default: Date.now},
-    edok : String
-    // notReadBy: []
+    edok : String,
+    notReadBy: {type: Array/* default: s()*/}
 });
 
 module.exports = mongoose.model('Snyts', SnytSchema);
