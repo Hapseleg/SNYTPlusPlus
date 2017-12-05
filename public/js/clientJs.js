cbe2d52d7dee50c53fe668ac924dee9370f5888d
Nicolaj Lundtoft Jørgensen <hapse@hh4.dk>
2017-12-05 12:11:04 +0100

    små fixes sort etc

--- public/js/clientJs.js
+++ public/js/clientJs.js
@@ -45,6 +45,7 @@ function regularSearch() {
                     var date = documents[i].created.toString().substring(0, 10);
                     var initials = documents[i].user;
                     var read = "";
+                    var category = documents[i].category;
                     if(documents[i].readBy) {
                         if(documents[i].readBy.includes(userId)) {
                             read = "X";
@@ -53,7 +54,7 @@ function regularSearch() {
                         }
                     }
                     //TODO add   <td>" + category + "</td> ???
-                    table.append("<tr class='clickableRow' data-href=" + documents[i]._id + "><td>" + subject + "</td><td>" + date + "</td><td>" + initials + "</td><td>" + read + "</td></tr>");
+                    table.append("<tr class='clickableRow' data-href=" + documents[i]._id + "><td>" + subject + "</td><td>" + category + "</td><td>" + date + "</td><td>" + initials + "</td><td>" + read + "</td></tr>");
                 }
                 $(".clickableRow").click(function() {
                     window.location.href = "http://localhost:1337/snyt/" + $(this).data("href");
@@ -107,6 +108,7 @@ function advancedSearch() {
             var date = documents[i].created.toString().substring(0, 10);
             var initials = documents[i].user;
             var read = "//TODO";
+            var category = documents[i].category;
             if(documents[i].readBy) {
                 if(documents[i].readBy.includes(userId)) {
                     read = "X";
@@ -115,7 +117,7 @@ function advancedSearch() {
                 }
             }
             //TODO add   <td>" + category + "</td> ???
-            table.append("<tr class='clickableRow' data-href=" + documents[i]._id + "><td>" + subject + "</td><td>" + date + "</td><td>" + initials + "</td><td>" + read + "</td></tr>");
+            table.append("<tr class='clickableRow' data-href=" + documents[i]._id + "><td>" + subject + "</td><td>" + category + "</td><td>" + date + "</td><td>" + initials + "</td><td>" + read + "</td></tr>");
         }
         $(".clickableRow").click(function() {
             window.location = "/snyt/" + $(this).data("href");
@@ -258,4 +260,60 @@ function updateUser() {
 }
 
 function deleteUser() {
+}
+
+//https://www.w3schools.com/howto/howto_js_sort_table.asp
+function sortTable(n) {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementsByClassName("table");
+    switching = true;
+    // Set the sorting direction to ascending:
+    dir = "asc";
+    /* Make a loop that will continue until
+    no switching has been done: */
+    while (switching) {
+        // Start by saying: no switching is done:
+        switching = false;
+        rows = table[0].getElementsByTagName("tr");
+        /* Loop through all table rows (except the
+        first, which contains table headers): */
+        for (i = 1; i < (rows.length - 1); i++) {
+            // Start by saying there should be no switching:
+            shouldSwitch = false;
+            /* Get the two elements you want to compare,
+            one from current row and one from the next: */
+            x = rows[i].getElementsByTagName("td")[n];
+            y = rows[i + 1].getElementsByTagName("td")[n];
+            /* Check if the two rows should switch place,
+            based on the direction, asc or desc: */
+            if (dir == "asc") {
+                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                    // If so, mark as a switch and break the loop:
+                    shouldSwitch= true;
+                    break;
+                }
+            } else if (dir == "desc") {
+                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                    // If so, mark as a switch and break the loop:
+                    shouldSwitch= true;
+                    break;
+                }
+            }
+        }
+        if (shouldSwitch) {
+            /* If a switch has been marked, make the switch
+            and mark that a switch has been done: */
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            // Each time a switch is done, increase this count by 1:
+            switchcount ++;
+        } else {
+            /* If no switching has been done AND the direction is "asc",
+            set the direction to "desc" and run the while loop again. */
+            if (switchcount == 0 && dir == "asc") {
+                dir = "desc";
+                switching = true;
+            }
+        }
+    }
 }
