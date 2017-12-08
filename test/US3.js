6b7a6e391aab4213281d2f9b06ca246849c04576
Casper Hedegaard <averagejoedk@gmail.com>
2017-12-08 08:37:05 +0100

    US3 + US6 test

---
+++ test/US3.js
@@ -0,0 +1,54 @@
+let assert = require('chai').assert;
+let request = require('supertest');
+let app = require("../app").app;
+let shutdown = require("../app").shutdown;
+
+let agent = request.agent(app);
+
+let login = {
+    user : {
+        email : "hejmeddig@hej.dk",
+        password : "123"
+    }
+};
+
+describe("Opret snyt", function() {
+
+    before(function(done) {
+        agent.post("/")
+            .type("form")
+            .send(login)
+            .end(function(err, res) {
+                done();
+            });
+    });
+
+    it("Kan oprette en SNYT", function(done) {
+
+        let snyt = {
+            snyt : {
+                subject: "Opret snyt testting",
+                category : "Logistik",
+                text : "Den her SNYT er lavet for at se om testen til at oprette en SNYT virker",
+                user : "CKH",
+                edok : "",
+                created : Date.now()
+            }
+        };
+
+        agent.post("/opretsnyt")
+            .type("form")
+            .send(snyt)
+            .end(function(err, res) {
+                assert.isNull(res.text.match(new RegExp(/html/, "g")));
+                done();
+            });
+    });
+
+    after(function(done) {
+        agent.get('/logout')
+            .end(function(err, res) {
+                done();
+            });
+    });
+});
---
+++ test/US6.js
@@ -0,0 +1,66 @@
+let assert = require('chai').assert;
+let request = require('supertest');
+let app = require("../app").app;
+let shutdown = require("../app").shutdown;
+
+let agent = request.agent(app);
+
+let login = {
+    user : {
+        email : "hejmeddig@hej.dk",
+        password : "123"
+    }
+};
+
+let snytEditId = "5a2a131b6835fd44b4476359";
+
+describe("Rediger snyt", function() {
+
+    before(function(done) {
+        agent.post("/")
+            .type("form")
+            .send(login)
+            .end(function(err, res) {
+                done();
+            });
+    });
+
+    it("Kan redigere en SNYT", function(done) {
+
+        let snyt = {
+            snyt : {
+                subject: "Opret snyt testting",
+                category : "Logistik",
+                text : "Den her SNYT er lavet for at se om testen til at oprette en SNYT virker",
+                user : "CKH",
+                edok : "",
+                created : "2017-12-08T04:20:43.454Z"
+            }
+        };
+
+        agent.post("/editSnyt")
+            .type("form")
+            .send({
+                snyt : {
+                    subject : snyt.snyt.subject,
+                    category : snyt.snyt.category,
+                    text : snyt.snyt.text + " update",
+                    user : "CKH",
+                    edok : "",
+                    _id : snytEditId,
+                    created : snyt.snyt.created
+                }
+            })
+            .end(function(err, res) {
+                assert.isNotNull(res.text.match(new RegExp(/html/, "g")));
+                done();
+            });
+    });
+
+    after(function(done) {
+        agent.get('/logout')
+            .end(function(err, res) {
+                done();
+            });
+    });
+});
